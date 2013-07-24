7fd2a9b41c0a08248ede32f555d6e4595924b380
swaraj <sbanerjee@zynga.com>
2013-07-23 23:34:24 -0700

    play soundcloud links entered through selector

--- public/player.js
+++ public/player.js
@@ -1,9 +1,39 @@
 $(document).ready(function(){
 	var scIFrame = $("#sc-widget")[0];
 	var scWidget = SC.Widget(scIFrame);
- 
+	var scLoaded = false;
+
+	var socket = io.connect('http://localhost:3700');
 	scWidget.bind(SC.Widget.Events.READY, function(){
-		
+		scLoaded = true;
 	});
+
+	socket.on('playLink', function(data){
+		var type = data.type;
+		var link = data.link;
+		if(type && link){
+			pauseAll();
+			switch(type){
+				case 'soundcloud':
+					playSoundCloudLink(link);
+					break;
+				default:
+					break;
+			}
+
+		}	
+	});
+
+	var playSoundCloudLink = function(link){
+		if(scLoaded){
+			scWidget.load(link, {auto_play: true});	
+		}
+	}
+
+	var pauseAll = function(){
+		if(scLoaded){
+			scWidget.pause();	
+		}
+	};
 });
 
